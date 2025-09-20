212ece125f3b2d8885e1c577a8041b4965b7b883
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2025-09-20 19:16:20 +0430

    button added and used

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>The react native great text component!</Text>
+      <Button title="Click Me" onPress={()=> alert("Button Clicked")}/>
     </View>
   );
 }
